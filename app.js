7dda6bde39738410f85a2d0848875d953a85a8f4
Sacha Sedriks <sachased@gmail.com>
2020-06-27 10:12:39 -0700

    added search query to /books route handler.

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ const app = express();
 
 app.use(morgan('common'));
 
+const books = require('./books-data.js')
+
 app.get('/books', (req, res) => {
-  // to be or not to be, that is the question
-})
+  
+  const { search = '' } = req.query; 
+  
+  // ^^^^ setting default query parameter for search.
+  // Then we can implement a filter function on books. To make the search case insensitive lowercase the title and the search before comparison. The includes() method of String used like this:
+  // str.includes(searchString)
+
+  let results = books
+    .filter(book =>      // NOTE the filter + includes method combo
+      book 
+        .title
+        .toLowerCase()
+        .includes(search.toLowerCase())
+      );
+
+  res
+    // .json(books);
+    .json(results);
+});
 
 app.listen(8000, () => {
   console.log('Express Server running on PORT 8000...')
-})
+});
